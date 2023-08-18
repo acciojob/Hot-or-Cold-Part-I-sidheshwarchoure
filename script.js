4c18231542d7655712683f413abdcd7d1523cba8
sidheshwarchoure <92964594+sidheshwarchoure@users.noreply.github.com>
2023-08-18 18:23:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,45 @@
 //your code here
+const secretNumber = Math.floor(Math.random() * 100) + 1;
+let previousGuess = null;
+
+const guessInput = document.getElementById('guessInput');
+const guessButton = document.getElementById('guessButton');
+const response = document.getElementById('response');
+
+guessButton.addEventListener('click', () => {
+  const guessedNumber = parseInt(guessInput.value);
+
+  if (isNaN(guessedNumber)) {
+    response.textContent = "Please enter a valid number.";
+    return;
+  }
+
+  if (previousGuess === null) {
+    previousGuess = guessedNumber;
+    response.textContent = `Guess higher or lower than ${guessedNumber}.`;
+  } else {
+    const prevDifference = Math.abs(secretNumber - previousGuess);
+    const currentDifference = Math.abs(secretNumber - guessedNumber);
+
+    if (currentDifference < prevDifference) {
+      response.textContent = "Getting hotter!";
+    } else {
+      response.textContent = "Getting colder!";
+    }
+
+    if (guessedNumber < secretNumber) {
+      response.textContent += " Guess higher.";
+    } else if (guessedNumber > secretNumber) {
+      response.textContent += " Guess lower.";
+    } else {
+      response.textContent = "Congratulations! You guessed the secret number.";
+      guessInput.disabled = true;
+      guessButton.disabled = true;
+    }
+
+    previousGuess = guessedNumber;
+  }
+
+  guessInput.value = "";
+});
 
